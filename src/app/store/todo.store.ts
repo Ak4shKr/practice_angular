dcbaa2a455b5587700f1df9e7b4451f36b15891b
ak4shkr <akashkmr7667@gmail.com>
2025-06-06 17:03:31 +0530

    implememted and utilise withHooks in ngrxsignal

--- src/app/store/todo.store.ts
+++ src/app/store/todo.store.ts
@@ -1,22 +1,15 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { getState, patchState, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
 import { ToDo } from '../utils/models/types';
-import { computed } from '@angular/core';
+import { computed, effect } from '@angular/core';
 
 export interface Todos {
   todos: ToDo[];
 }
 
 export const initialTodos: Todos = {
-  todos: loadFromStorage() || [],
+  todos: [],
 };
 
-function loadFromStorage(): ToDo[] {
-  return JSON.parse(localStorage.getItem('todos') || '[]');
-}
-
-function saveToStorage(todos: ToDo[]) {
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
 
 export const todoStore = signalStore(
   {
@@ -30,19 +23,29 @@ export const todoStore = signalStore(
     addTodo: (todo: ToDo) => {
       const updatedTodos = [...todos(), todo];
       patchState(store, { todos: updatedTodos });
-      saveToStorage(updatedTodos);
     },
     removeTodo: (todoId: number) => {
       const updatedTodos = todos().filter((t: ToDo) => t.id !== todoId);
       patchState(store, { todos: updatedTodos });
-        saveToStorage(updatedTodos);
     },
     updateTodo: (updatedTodo: ToDo) => {
       const updatedTodos = todos().map((t: ToDo) =>
         t.id === updatedTodo.id ? updatedTodo : t
       );
       patchState(store, { todos: updatedTodos });
-        saveToStorage(updatedTodos);
     },
-  }))
+  })),
+  withHooks({
+    onInit(store){
+        const initialTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+        if (initialTodos) {
+            patchState(store, { todos: initialTodos });
+        }
+        effect(() => {
+            const state = getState(store);
+            localStorage.setItem('todos', JSON.stringify(state.todos));
+        });
+    }
+  })
+
 );
