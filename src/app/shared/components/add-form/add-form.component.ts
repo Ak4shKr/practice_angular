17e412e2912303952a6afea64c20d69a9560c8f8
ak4shkr <akashkmr7667@gmail.com>
2025-06-10 12:24:23 +0530

    reactive form validation check and updated

--- src/app/shared/components/add-form/add-form.component.ts
+++ src/app/shared/components/add-form/add-form.component.ts
@@ -9,24 +9,24 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { todoStore } from '../../../store/todo.store';
 import { ToDo } from '../../../utils/models/types';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-add-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,ButtonModule, InputTextModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, InputTextModule],
   templateUrl: './add-form.component.html',
 })
 export class AddFormComponent implements OnInit {
   todoForm: FormGroup;
-  todoId: number | null = null; 
+  todoId: number | null = null;
 
   loadingButton: boolean = false;
 
   // title:string ="";
-  
-  todoStore = inject(todoStore)
+
+  todoStore = inject(todoStore);
   todos: ToDo[] = this.todoStore.todos();
 
   constructor(
@@ -36,20 +36,24 @@ export class AddFormComponent implements OnInit {
   ) {
     this.todoForm = this.fb.group({
       title: ['', Validators.required],
-      description: [''],
+      description: ['', { validators: [Validators.required, Validators.minLength(10)] }],
       priority: ['medium', Validators.required],
+    });
+    this.todoForm.valueChanges.subscribe((value) => {
+      console.log('Form value changed:', value);
     });
   }
 
   ngOnInit(): void {
     const idParam = this.route.snapshot.paramMap.get('id');
     if (idParam) {
-      this.todoId = +idParam;
+      this.todoId = Number(idParam);
       const item = this.todos.find((i) => i.id === this.todoId);
       if (item) {
         this.todoForm.patchValue(item);
       }
     }
+    console.log('Current todos:', this.todoForm);
   }
 
   onSubmit() {
@@ -61,7 +65,7 @@ export class AddFormComponent implements OnInit {
     const formData = this.todoForm.value;
 
     if (this.todoId) {
-      this.todoStore.updateTodo({...formData, id: this.todoId });
+      this.todoStore.updateTodo({ ...formData, id: this.todoId });
     } else {
       const newItem = {
         ...formData,
@@ -69,6 +73,6 @@ export class AddFormComponent implements OnInit {
       };
       this.todoStore.addTodo(newItem);
     }
-    this.router.navigate(['/']); 
+    this.router.navigate(['/']);
   }
 }
